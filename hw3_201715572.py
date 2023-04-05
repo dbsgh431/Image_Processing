34f9c1c45bb4d1a45fee457a914926efd9fc9b72
YOONHO <you@example.com>
2023-04-05 21:00:32 +0900

    프레임에 한글 text 추가, 영상 전환 기능 구현

--- hw3_201715572.py
+++ hw3_201715572.py
@@ -1,5 +1,15 @@
 import cv2
 import numpy as np
+from PIL import ImageFont, ImageDraw, Image
+
+def putText_factory(src):
+    font = ImageFont.truetype("fonts/gulim.ttc",30)
+    org = (20,20)
+    text  = "201715572 김윤호"
+    src = Image.fromarray(src)
+    draw = ImageDraw.Draw(src)
+    draw.text(org, text, font=font,fill=(0,255,255))
+    return np.array(src)
 
 src1 = cv2.imread('data/lec9_Resources/lec9_airplane.bmp')
 src2 = cv2.imread('data/lec9_Resources/lec9_field.bmp')
@@ -11,12 +21,20 @@ h, w, _ = src1.shape
 fourcc = cv2.VideoWriter_fourcc(*"DIVX")
 out = cv2.VideoWriter('lec6_output1.avi', fourcc, fps, (w,h))
 
+#1 Todo name, info text  
 
 for i in range(fps*1):
+    src2 = putText_factory(src2)
     out.write(src2)
-for i in range(fps*3):
-    out.write(src2+ src1)
-for i in range(fps*1):
+for j in range(fps*3):
+    #2 Todo : weighted summation
+    beta = float(j / (fps*3))
+    alpha = (1 - beta)
+    dst = cv2.addWeighted(src2,alpha,src1,beta,gamma=None)
+    dst = putText_factory(dst)
+    out.write(dst)
+for k in range(fps*1):
+    src1 = putText_factory(src1)
     out.write(src1)
 
 
