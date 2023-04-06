281ddd6f488ed4972d089f69fe84c68c23b22918
YOONHO <you@example.com>
2023-04-06 13:05:57 +0900

    output 파일명 수정

--- hw3_201715572.py
+++ hw3_201715572.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from PIL import ImageFont, ImageDraw, Image
 
+#1 Todo name, info text 
 def putText_factory(src):
     font = ImageFont.truetype("fonts/gulim.ttc",30)
     org = (20,20)
@@ -19,15 +20,15 @@ fps = 30
 h, w, _ = src1.shape
 
 fourcc = cv2.VideoWriter_fourcc(*"DIVX")
-out = cv2.VideoWriter('lec6_output1.avi', fourcc, fps, (w,h))
 
-#1 Todo name, info text  
+out = cv2.VideoWriter('hw3_201715572.avi', fourcc, fps, (w,h))
+ 
 
 for i in range(fps*1):
     src2 = putText_factory(src2)
     out.write(src2)
 for j in range(fps*3):
-    #2 Todo : weighted summation
+    #2 Todo : weighted sum
     beta = float(j / (fps*3))
     alpha = (1 - beta)
     dst = cv2.addWeighted(src2,alpha,src1,beta,gamma=None)
@@ -36,6 +37,5 @@ for j in range(fps*3):
 for k in range(fps*1):
     src1 = putText_factory(src1)
     out.write(src1)
-
 
 out.release()
